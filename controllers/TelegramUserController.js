348553c4010bb7762fae088c8bd9594979620c39
ELHart05 <o.allaoua@esi-sba.dz>
2023-07-28 15:08:20 +0100

    fix telegram check user issue

--- controllers/TelegramUserController.js
+++ controllers/TelegramUserController.js
@@ -7,15 +7,23 @@ const getTelegramUser = async (req, res, next) => {
 
         if (username === undefined) {
             throw new AppError("Oh!, User can't be retrieved...", 404);
-        } else {
-            let result = await TelegramUser.findOne({username}); //example: http://localhost:4000/auth/telegram?username=${username}
-            if (!result) {
-                throw new AppError("Oh!, User not found...", 404);
-            }
-            res.status(200).send(result);
         }
 
-        res.send(result);
+        let result = await TelegramUser.findOne({username}); //example: http://localhost:4000/auth/telegram?username=${username}
+        
+        if (!result) {
+            throw new AppError("Oh!, User not found...", 404);
+        }
+
+        if (!result.joinedEM) {
+            throw new AppError("User didn't join EarthMeta group...", 400);
+        }
+    
+        if (result.tookReward) {
+            throw new AppError("Already took reward...", 400);
+        }
+
+        res.status(200).send({telegramID: result.telegramID});
 
     } catch (err) {
         next(err);
